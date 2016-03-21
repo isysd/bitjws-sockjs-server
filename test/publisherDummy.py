03f9dd75bf0d60043b60b488684b137df7773380
Marco Agner <marcoagner.dev@gmail.com>
2016-03-21 13:31:05 -0300

    updates publisher dummy to bitjws

--- test/publisherDummy.py
+++ test/publisherDummy.py
@@ -3,8 +3,7 @@ import sys
 import json
 import pika
 
-from bitcoin.wallet import CKey, P2PKHBitcoinAddress
-from mrest_core.auth.message import *
+import bitjws
 
 configdir = os.environ.get('SOCKJS_MQ_CONFIG_DIR', '../')
 if configdir not in sys.path:
@@ -22,15 +21,20 @@ def publish(message):
     """
     :param message: a compact signed mrest message
     """
-    pikaChannel.basic_publish(body=json.dumps(message),
+    pikaChannel.basic_publish(body=message,
                               exchange=pikaconfig.EXCHANGE['exchange'],
                               routing_key='')
 
-privkey = CKey(os.urandom(64))
-pubhash = str(P2PKHBitcoinAddress.from_pubkey(privkey.pub))
+privkey = bitjws.PrivateKey()
+pubhash = bitjws.pubkey_to_addr(privkey.pubkey.serialize())
 
 mdata = {'metal': 'testinium', 'mint': 'publisherDummy.py'}
-heads, pmdata = prepare_mrest_message('RESPONSE', data=mdata, pubhash=pubhash, privkey=privkey,
-                                       headers={}, permissions=['authenticate'])
-escm = encode_compact_signed_message('RESPONSE', pmdata, heads, 'coin')
-publish(escm)
+msg = bitjws.sign_serialize(privkey,
+                            metal=mdata['metal'],
+                            mint=mdata['mint'],
+                            pubhash=pubhash,
+                            headers={},
+                            permissions=['authenticate'],
+                            method='RESPONSE',
+                            model='coin')
+publish(msg)
